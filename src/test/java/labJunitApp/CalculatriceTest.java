550d562d8457b25975876db74e36f89de5b5ac30
merouane-dev <merouanebeladam@gmail.com>
2024-04-19 11:48:03 +0200

    Your commit message here

---
+++ src/test/java/labJunitApp/CalculatriceTest.java
@@ -0,0 +1,14 @@
+package labJunitApp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatriceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
